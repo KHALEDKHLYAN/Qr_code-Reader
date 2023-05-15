2ead6dfe4b23d3c3bdc29afc94d57644d2004931
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-05-15 19:42:23 +0300

    printing ints

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import numpy as np
 from pyzbar.pyzbar import decode
 
 img = cv2.imread('ar.png')
-code = decode(img)
-print(code)
+# code = decode(img)
+# print(code)
+
+for barcode in decode(img):
+    print(barcode.polygon)
