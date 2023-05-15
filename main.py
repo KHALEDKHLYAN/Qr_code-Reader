9943a6a2478a5ea602b4046ce952e45a04cecf6f
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-05-15 20:05:33 +0300

    open camera

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-img = cv2.imread('ar.png')
+# img = cv2.imread('ar.png')
 # code = decode(img)
 # print(code)
 
-for barcode in decode(img):
-    print(barcode.data)
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
-    myData = barcode.data.decode('utf-8')
-    print(myData)
+while True:
+
+    Success, img = cap.read()
+
+    for barcode in decode(img):
+        print(barcode.data)
+
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+
+        cv2.imshow('Result',img)
+        cv2.waitKey(1)
